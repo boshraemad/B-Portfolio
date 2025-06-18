c9a24a7c182b8b5c8e4d077d2f2cfb37106f527d
boshraemad <boshraemad2004@icloud.com>
2025-06-18 07:09:15 +0300

    fix  small screens toggle menu

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,7 +1,13 @@
 import NavBar from "../components/navbar";
 import HomePageLayout from "./homePageLayout";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { initFlowbite } from "flowbite";
+
 function HomePage(){
+   useEffect(()=>{
+      initFlowbite();
+   })
     return(
        <div>
           <NavBar/>
