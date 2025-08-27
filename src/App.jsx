d433cef193b1a3b9e0254d584e50166eadbb5795
boshraemad <boshraemad2004@icloud.com>
2025-08-27 23:27:05 +0300

    fix : redirect to home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,10 @@ import HomePageLayout from "./pages/homePageLayout";
 import Projects from "./pages/projects";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useNavigate } from "react-router";
-import { useState, useEffect, useCallback } from "react";
-import { supabase } from "./api/supabase-client";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [session, setSession] = useState(null);
-  const navigate = useNavigate();
-
-  const fetchSession = useCallback(
-    () => async () => {
-      const { data } = await supabase.auth.getSession();
-      setSession(data.session);
-      if (session) {
-        navigate("/home");
-      }
-    },
-    [session]
-  );
-
-  useEffect(() => {
-    fetchSession();
-    //listen for login
-    const { data: listener } = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        if (session) {
-          navigate("/home");
-        }
-      }
-    );
-  }, [fetchSession]);
-
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
