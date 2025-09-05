e379cff5841e31216c88680c9238928671a3f201
boshraemad <boshraemad2004@icloud.com>
2025-09-05 08:23:20 +0300

    update likes

--- src/features/projects/ProjectCard.jsx
+++ src/features/projects/ProjectCard.jsx
@@ -1,16 +1,18 @@
-import { FaRegHeart } from "react-icons/fa6";
+import { FaRegHeart , FaHeart } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { TbWorld } from "react-icons/tb";
 import useUpdateLikes from "./useUpdateLikes";
-import { useState } from "react";
+
 export default function ProjectCard({project}) {
     const {id , name , image , repo , demo , likes , tags} = project;
-    const [liked , setLiked] = useState();
-    const updateLikes = useUpdateLikes();
+    const updateLikes = useUpdateLikes(id);
+    const likedProjects = JSON.parse(localStorage.getItem("likedProjects"));
+    const isLiked = likedProjects?.includes(id);
+
   return (
-    <div className="max-w-sm w-full rounded-2xl bg-white/5 backdrop-blur-lg shadow-lg p-3 text-white/80">
+    <div className="w-full rounded-2xl bg-white/5 backdrop-blur-lg shadow-lg p-3 text-white/80">
         <img src={image} className="object-cover rounded-sm"/>
-        <div className="flex justify-between items-center px-3"><p className="font-semibold mt-2">{name}</p><div className="flex items-center gap-2"><span>{likes}</span><FaRegHeart onClick={()=>updateLikes({id:id , likes:liked ? likes-1 : likes+1})} className="cursor-pointer"/></div></div>
+        <div className="flex justify-between items-center px-3"><p className="font-semibold mt-2">{name}</p><div className="flex items-center gap-2"><span>{likes}</span>{!isLiked ? <FaRegHeart onClick={()=>updateLikes({id:id , likes:likes+1})} className={`cursor-pointer`}/> :<FaHeart onClick={()=>updateLikes({id:id , likes:likes-1})} className={`cursor-pointer text-red-600`}/>} </div></div>
         <div className="mt-6 flex items-center gap-3 px-3 mb-2">
             <a className="text-xl text-white" href={repo}><FaGithub/></a>
             <a className="text-xl text-blue-600" href={demo}><TbWorld/></a>
--- src/features/projects/useUpdateLikes.js
+++ src/features/projects/useUpdateLikes.js
@@ -1,8 +1,20 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation , useQueryClient } from "@tanstack/react-query"
 import { updateLikes as update } from "../../api/projects-services"
-export default function useUpdateLikes() {
+export default function useUpdateLikes(id) {
+  const queryClient = useQueryClient();
   const {mutate:updateLikes} = useMutation({
-    mutationFn:(data)=>update(data)
+    mutationFn:(data)=>update(data),
+    onSuccess:()=>{
+      queryClient.invalidateQueries(["projects"])
+      const likedProjects=JSON.parse(localStorage.getItem("likedProjects"));
+      if(!likedProjects.includes(id)){
+        localStorage.setItem("likedProjects" , JSON.stringify([...likedProjects , id]));
+      }else{
+        const updatedLikedProjects = likedProjects.filter(projectId=>projectId!==id);
+        localStorage.setItem("likedProjects" , JSON.stringify(updatedLikedProjects));
+      }
+      
+    }
   })
 
   return updateLikes
